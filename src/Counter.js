a8a668accb4e3dbfcad1c9608c805c32f3efe158
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-27 11:25:29 +0300

    Updating local storage after updating state.

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 
+const getStateFromLocalStorage = () => {
+  const storage = localStorage.getItem('counterState');
+
+  if (storage) return JSON.parse(storage);
+  return { count: 0 };
+};
+
+document.title = 'hello';
+
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getStateFromLocalStorage();
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState(
+      (state, props) => {
+        if (state.count >= props.max) return;
+        return { count: state.count + props.step };
+      },
+      () => localStorage.setItem('counterState', JSON.stringify(this.state)),
+    );
+
+    console.log('after');
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  reset() {
+    // this.setState({ count: this.state.count + 1 });
+    this.setState({ count: 0 });
+  }
+
   render() {
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{this.state.count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
